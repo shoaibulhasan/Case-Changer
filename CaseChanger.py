b8e92103a909afb7e21d2b16e3fff23e64274e4c
Shoaib Ul Hassan <107319678+shoaibulhasan@users.noreply.github.com>
2023-11-08 00:48:38 +0500

    Change letter case

---
+++ CaseChanger.py
@@ -0,0 +1,8 @@
+"""What's your favourite language? english
+My favourite language is  ENGLISH
+My favourite language is  english
+"""
+print("-------== Case Change ==--------")
+user_Input_Str = input("What's your favourite language? ")
+print("My favourite language is ", user_Input_Str.upper())
+print("My favourite language is ", user_Input_Str.lower())
